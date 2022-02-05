e721220a2a4d4839c30f8887f80d6e021ef6ee91
chorobaev <chorobaev.nurbol@gmail.com>
2022-02-06 05:48:49 +0600

    add empty state item to ExamListAdapter

--- mesh-exam/android-base/src/main/java/io/flaterlab/meshexam/androidbase/common/adapter/ExamListAdapter.kt
+++ mesh-exam/android-base/src/main/java/io/flaterlab/meshexam/androidbase/common/adapter/ExamListAdapter.kt
@@ -13,25 +13,50 @@ import javax.inject.Inject
 
 class ExamListAdapter @Inject constructor(
 
-) : ListAdapter<ExamListAdapter.ExamItem, ExamListAdapter.ViewHolder>(DIF_CALLBACK) {
+) : ListAdapter<ExamListAdapter.ExamItem, RecyclerView.ViewHolder>(DIF_CALLBACK) {
 
     var onExamClickListener: (ExamItem) -> Unit = {}
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(parent).apply {
-            itemView.clickWithDebounce {
-                runCatching { getItem(adapterPosition) }.getOrNull()?.let(onExamClickListener)
+    override fun submitList(list: List<ExamItem>?) {
+        super.submitList(if (list.isNullOrEmpty()) listOf(ExamItem.Companion.Empty) else list)
+    }
+
+    override fun submitList(list: List<ExamItem>?, commitCallback: Runnable?) {
+        super.submitList(
+            if (list.isNullOrEmpty()) listOf(ExamItem.Companion.Empty) else list,
+            commitCallback
+        )
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when {
+            getItem(position).id == ExamItem.EMPTY_ITEM_ID -> VIEW_TYPE_EMPTY
+            else -> super.getItemViewType(position)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_EMPTY -> SimpleViewHolder(parent, R.layout.item_empty)
+            else -> ViewHolder(parent).apply {
+                itemView.clickWithDebounce {
+                    runCatching { getItem(adapterPosition) }.getOrNull()?.let(onExamClickListener)
+                }
             }
         }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(getItem(position))
-        holder.itemView.applyLayoutParams<RecyclerView.LayoutParams> {
-            bottomMargin = if (position == itemCount - 1) {
-                holder.itemView.resources.getDimension(R.dimen.margin_medium).toInt()
-            } else {
-                0
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ViewHolder -> {
+                holder.bind(getItem(position))
+                holder.itemView.applyLayoutParams<RecyclerView.LayoutParams> {
+                    bottomMargin = if (position == itemCount - 1) {
+                        holder.itemView.resources.getDimension(R.dimen.margin_medium).toInt()
+                    } else {
+                        0
+                    }
+                }
             }
         }
     }
@@ -54,13 +79,29 @@ class ExamListAdapter @Inject constructor(
         }
     }
 
+    class SimpleViewHolder(parent: ViewGroup, layoutResId: Int) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context).inflate(layoutResId, parent, false)
+    )
+
     interface ExamItem {
         val id: String
         val name: String
         val durationInMin: Long
+
+        companion object {
+            internal const val EMPTY_ITEM_ID = "EMPTY_ITEM_ID"
+
+            internal object Empty : ExamItem {
+                override val id: String = EMPTY_ITEM_ID
+                override val durationInMin: Long = 0
+                override val name: String = ""
+            }
+        }
     }
 
     companion object {
+        private const val VIEW_TYPE_EMPTY = 1
+
         private val DIF_CALLBACK = object : DiffUtil.ItemCallback<ExamItem>() {
             override fun areItemsTheSame(oldItem: ExamItem, newItem: ExamItem): Boolean {
                 return oldItem.id == newItem.id
